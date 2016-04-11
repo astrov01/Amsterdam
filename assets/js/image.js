3b3094556aba7ca3dc10aa52c04787859d45df1d
astrov01 <vic.espinozac@gmail.com>
2016-04-11 20:52:18 +0200

    Ultimas modificaciones

---
+++ assets/js/image.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Victor Espinoza on 11/04/2016.
+ */
+function image() {
+    var img = document.createElement("IMG");
+    img.src = "/../../images/plano-metro.png";
+    $('#image').html(img);
+}
